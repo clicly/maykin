d1c3e012618ff673ee128b758e0108f825b082b8
Lily Perera <lilycapetillo86@gmail.com>
2021-08-25 13:22:30 -0400

    Add models to the Admin system

--- back/admin.py
+++ back/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from maykin.back.models import *
+
 # Register your models here.
+
+admin.site.register(City) # Add Models to the admin
+admin.site.register(Hotel) # Add Models to the admin
+
