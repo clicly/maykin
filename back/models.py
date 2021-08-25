5041f37d970dbb6091707343e61f84ab7fe7b241
Lily Perera <lilycapetillo86@gmail.com>
2021-08-25 13:59:57 -0400

    Ordering the Hotel Model by the City

--- back/models.py
+++ back/models.py
@@ -1,43 +1,43 @@
 from django.db import models
 
-
 # Create your models here.
 
 # City Model
 
 class City(models.Model):
-    code = models.CharField(max_length=3, verbose_name='Code', unique=True)  # City Code
-    name = models.CharField(max_length=150, verbose_name='City')  # City Name
+    code = models.CharField(max_length=3, verbose_name='Code', unique=True)  # city Code
+    name = models.CharField(max_length=150, verbose_name='city')  # city Name
 
     class Meta:
-        verbose_name = "City"
+        verbose_name = "city"
         verbose_name_plural = "Citys"
-        ordering = ['code']  # Ordering by City Code
+        ordering = ['code']  # Ordering by city Code
 
     def __str__(self):
         return self.name
 
 
-    # Get full City
+    # Get full city
     def get_full_city(self):
         return '{} : {}'.format(self.code, self.name)  # Return complete city ( Code : Name )
 
 
 # Hotel Model
 class Hotel(models.Model):
-    cit = models.ForeignKey(City, on_delete=models.CASCADE)  # Relationship with City
+    cit = models.ForeignKey(City, on_delete=models.CASCADE)  # Relationship with city
     hotel_code = models.CharField(max_length=5, verbose_name='Hotel Code', unique=True)  # Hotel Code
     hotel_name = models.CharField(max_length=150, verbose_name='Hotel Name')  # Hotel Name
 
     class Meta:
         verbose_name = "Hotel"
         verbose_name_plural = "Hotels"
+        ordering = ['cit']  # Ordering by city Code
 
     def __str__(self):
         return self.hotel_name
 
     # Get full Hotel
     def get_full_hotel(self):
-        return '{} : {} : {}'.format(self.cit, self.hotel_name, self.hotel_name)  # Return complete Hotel ( City : Code : Name )
+        return '{} : {} : {}'.format(self.cit, self.hotel_name, self.hotel_name)  # Return complete Hotel ( city : Code : Name )
 
 
